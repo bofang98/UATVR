62a7f47714bd6a11c52df2e86ec78f74a5646b9b
bofang98 <42595629+bofang98@users.noreply.github.com>
2023-11-05 14:05:59 +0800

    Add files via upload

---
+++ dataloaders/data_dataloaders.py
@@ -0,0 +1,56 @@
+import torch
+from torch.utils.data import DataLoader
+from dataloaders.dataloader_msrvtt_retrieval import MSRVTT_DataLoader
+from dataloaders.dataloader_msrvtt_retrieval import MSRVTT_TrainDataLoader
+
+def dataloader_msrvtt_train(args, tokenizer):
+    msrvtt_dataset = MSRVTT_TrainDataLoader(
+        csv_path=args.train_csv,
+        json_path=args.data_path,
+        features_path=args.features_path,
+        max_words=args.max_words,
+        feature_framerate=args.feature_framerate,
+        tokenizer=tokenizer,
+        max_frames=args.max_frames,
+        unfold_sentences=args.expand_msrvtt_sentences,
+        frame_order=args.train_frame_order,
+        slice_framepos=args.slice_framepos,
+        strategy=args.strategy
+    )
+
+    train_sampler = torch.utils.data.distributed.DistributedSampler(msrvtt_dataset)
+    dataloader = DataLoader(
+        msrvtt_dataset,
+        batch_size=args.batch_size // args.n_gpu,
+        num_workers=args.num_thread_reader,
+        pin_memory=False,
+        shuffle=(train_sampler is None),
+        sampler=train_sampler,
+        drop_last=True,
+    )
+
+    return dataloader, len(msrvtt_dataset), train_sampler
+
+def dataloader_msrvtt_test(args, tokenizer, subset="test"):
+    msrvtt_testset = MSRVTT_DataLoader(
+        csv_path=args.val_csv,
+        features_path=args.features_path,
+        max_words=args.max_words,
+        feature_framerate=args.feature_framerate,
+        tokenizer=tokenizer,
+        max_frames=args.max_frames,
+        frame_order=args.eval_frame_order,
+        slice_framepos=args.slice_framepos
+    )
+    dataloader_msrvtt = DataLoader(
+        msrvtt_testset,
+        batch_size=args.batch_size_val,
+        num_workers=args.num_thread_reader,
+        shuffle=False,
+        drop_last=False,
+    )
+    return dataloader_msrvtt, len(msrvtt_testset)
+
+
+DATALOADER_DICT = {}
+DATALOADER_DICT["msrvtt"] = {"train":dataloader_msrvtt_train, "val":dataloader_msrvtt_test, "test":None}
